972b41fe0bb87fef81b8e0550e22c44efcb3c107
初時雨🍥 <Hatsushigure.c@gmail.com>
2024-11-30 15:06:24 +0800

    🐞 fix(issue1): Now tolerate errors
    Adds a deviation (default 5%), so small errors can be ignored
    
    fix #1

--- identify_dtmf.m
+++ identify_dtmf.m
@@ -1,27 +1,24 @@
 function key = identify_dtmf(frequencies)
-    dtmf_matrix = {
+    dtmfMatrix = {
         [697, 1209], '1';
         [697, 1336], '2';
         [697, 1477], '3';
-        [697, 1633], 'A';
         [770, 1209], '4';
         [770, 1336], '5';
         [770, 1477], '6';
-        [770, 1633], 'B';
         [852, 1209], '7';
         [852, 1336], '8';
         [852, 1477], '9';
-        [852, 1633], 'C';
         [941, 1209], '*';
         [941, 1336], '0';
-        [941, 1477], '#';
-        [941, 1633], 'D'
+        [941, 1477], '#'
     };
 
     key = 'Unknown';
-    for i = 1:size(dtmf_matrix, 1)
-        if all(ismember(dtmf_matrix{i, 1}, frequencies))
-            key = dtmf_matrix{i, 2};
+    for i = 1 : size(dtmfMatrix, 1)
+        stdFreq = dtmfMatrix{i, 1};
+        if all(((frequencies - stdFreq) ./ stdFreq) < 0.05) % Default deviation is 5%
+            key = dtmfMatrix{i, 2};
             break;
         end
     end
